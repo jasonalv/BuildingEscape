a9be5a6acb00c18e72dfb88bcebdd5239226d1a7
jasonalv <jsnalvarado@gmail.com>
2016-09-26 12:53:46 -0500

    Section 58 Message Handling

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -21,7 +21,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s!"), *ObjectName);
 	
 }
 
